35b801f4dc492ea472569213a06e27057606fa69
aswathisuseelan <30695607+aswathisuseelan@users.noreply.github.com>
2017-08-03 21:41:22 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,7 +9,7 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 app.get('/article-1', function (req, res) {
-  res.send(path.join('article one requested'));
+   res.sendFile(path.join(__dirname, 'ui', 'article-1.html'));
 });
 app.get('/article-2', function (req, res) {
   res.send(path.join('article two requested'));
