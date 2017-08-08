f78399acbcd5c66f206b0857fd4c01d25c6fe672
aswathisuseelan <30695607+aswathisuseelan@users.noreply.github.com>
2017-08-08 23:03:39 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -1,1 +1,7 @@
-console.log('Loaded!');
+var button=getElementById('counter');
+counter=0;
+button.onClick=function(){
+    counter=counter+1;
+    var span=document.getElementNById('count');
+    spanner.InnerHTML=counter.getString();
+}
