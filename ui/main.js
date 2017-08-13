f0f7231273887ff33157bfe9f45340a83ab6bc5e
aswathisuseelan <30695607+aswathisuseelan@users.noreply.github.com>
2017-08-13 23:33:30 +0530

    [imad-console] Updates ui/main.js

--- ui/main.js
+++ ui/main.js
@@ -6,3 +6,16 @@ button.onClick = function ()
     var span=document.getElementById('count');
     spanner.InnerHTML=counter.toString();
 }
+console.log('loaded');
+var marginLeft=0;
+var img=document.getElementById('madi');
+img.onclick=function(){
+    var interval=setInterval(moveRight,100);
+    img.style.marginLeft=100px;
+    
+};
+function moveRight(){
+    marginLeft=marginLeft+10;
+    img.style.marginLeft+margiLeft+'px';
+}
+
